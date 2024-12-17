f89c244ca95647f3af97de3e6e5eedfa973779d7
qian peng <qian.peng@mistcode.com>
2024-12-17 14:47:08 +1300

    fix(editor): fix force reset bug
    BUG: when parent widget setState, the state will force reset
    
    SOLUTION: properly handle the widget update

--- lib/src/editor/editor.dart
+++ lib/src/editor/editor.dart
@@ -105,7 +105,26 @@ class ExtendedImageEditorState extends State<ExtendedImageEditor> with SingleTic
     super.dispose();
   }
 
-  void _initGestureConfig() {
+  void _initGestureConfig([ExtendedImageEditor? oldWidget]) {
+    if (oldWidget != null &&
+        widget.extendedImageState.extendedImageInfo !=
+            oldWidget.extendedImageState.extendedImageInfo) {
+      // reset
+      _editActionDetails = null;
+    }
+    if (_editActionDetails == null) {
+      final int length = _history.length;
+      _history.clear();
+      if (length != _history.length) {
+        _safeUpdate(
+              () {
+            _editorConfig?.controller?._notifyListeners();
+          },
+        );
+      }
+    }
+    // check config
+    final EditorConfig? oldConfig = _editorConfig?.copyWith();
     _editorConfig = widget.extendedImageState.imageWidget.initEditorConfigHandler?.call(widget.extendedImageState) ?? EditorConfig();
     _editorConfig?.controller?._state = this;
 
@@ -124,14 +143,19 @@ class ExtendedImageEditorState extends State<ExtendedImageEditor> with SingleTic
       _editActionDetails!.cropAspectRatio = _editorConfig!.cropAspectRatio;
     }
 
-    final int length = _history.length;
-    _history.clear();
-    if (length != _history.length) {
-      _safeUpdate(
-        () {
-          _editorConfig!.controller?._notifyListeners();
-        },
-      );
+    if (oldConfig != null &&
+        oldConfig.cropAspectRatio != config.cropAspectRatio) {
+      updateCropAspectRatio(config.cropAspectRatio);
+    }
+    if (_editActionDetails!.totalScale > config.maxScale) {
+      _editActionDetails!.totalScale = config.maxScale;
+      _editActionDetails!.getFinalDestinationRect();
+      _recalculateCropRect();
+    }
+    if (oldConfig != null &&
+        oldConfig.cropRectPadding != config.cropRectPadding) {
+      _editActionDetails!.cropRectPadding = config.cropRectPadding;
+      _recalculateCropRect();
     }
 
     // save after afterPaintImage
@@ -139,12 +163,12 @@ class ExtendedImageEditorState extends State<ExtendedImageEditor> with SingleTic
     // _saveCurrentEditActionDetails();
   }
 
-  // @override
-  // void didUpdateWidget(ExtendedImageEditor oldWidget) {
-  //   // _editActionDetails = null;
-  //   // _initGestureConfig();
-  //   super.didUpdateWidget(oldWidget);
-  // }
+  @override
+  void didUpdateWidget(ExtendedImageEditor oldWidget) {
+    // _editActionDetails = null;
+     _initGestureConfig(oldWidget);
+    super.didUpdateWidget(oldWidget);
+  }
 
   @override
   Widget build(BuildContext context) {
